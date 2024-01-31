862283833cd59ef3cc0fdac12868d0e397750a5c
eriksalomon <116576276+erikdiazmilla@users.noreply.github.com>
2024-01-31 09:19:52 +0100

    Revert "Feat isak"

--- scripts/users.js
+++ scripts/users.js
@@ -1,20 +1,5 @@
 /* Här skapar ni en array bestående av minst 3 st användarobjekt */
 
 let users = [
-    {
-        username: 'Anton',
-        password: 'isbest'
-    },
-    {
-        username: 'Ammar',
-        password: 'isbetter'
-    },
-    {
-        username: 'Isak',
-        password: '123'
-    },
-    {
-        username: 'Erik',
-        password: 'istheverybest'
-    },
+    
 ];
